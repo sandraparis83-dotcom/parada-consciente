7b2f840c3b2efeaf4a640831be5eb38df0629a57
sandraparis83-dotcom <sandraparis83@gmail.com>
2026-07-11 16:44:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const boton = document.querySelector("button");
+const circulo = document.getElementById("circulo");
 
-boton.onclick = function() {
-    boton.innerHTML = "Respirá...";
+boton.onclick = function () {
+    boton.style.display = "none";
+
+    circulo.animate(
+        [
+            { transform: "scale(1)" },
+            { transform: "scale(1.4)" },
+            { transform: "scale(1)" }
+        ],
+        {
+            duration: 4000,
+            iterations: Infinity
+        }
+    );
 };
+
