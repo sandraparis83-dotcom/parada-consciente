3dbc18444ca7f3a15a50947009e2b9194b9fe39c
sandraparis83-dotcom <sandraparis83@gmail.com>
2026-07-12 11:41:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const boton = document.querySelector("button");
 const circulo = document.getElementById("circulo");
+const tiempo = document.getElementById("tiempo");
 
 boton.onclick = function () {
 
     boton.style.display = "none";
 
     let inhalando = true;
+    let segundos = 120;
+
+    actualizarTiempo();
 
     circulo.innerHTML = "INHALÁ";
     circulo.style.transform = "scale(1.4)";
@@ -23,6 +27,29 @@ boton.onclick = function () {
         inhalando = !inhalando;
 
     }, 6000);
+
+    const reloj = setInterval(function () {
+
+        segundos--;
+
+        actualizarTiempo();
+
+        if (segundos <= 0) {
+            clearInterval(reloj);
+            tiempo.innerHTML = "✔";
+            circulo.innerHTML = "¡Muy bien!";
+        }
+
+    }, 1000);
+
+    function actualizarTiempo() {
+        let min = Math.floor(segundos / 60);
+        let seg = segundos % 60;
+
+        if (seg < 10) seg = "0" + seg;
+
+        tiempo.innerHTML = min + ":" + seg;
+    }
 
 };
 
